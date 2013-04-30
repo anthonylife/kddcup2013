435fda91be6b0ece3b05b8b28c04a09005219aa8
anthonylife <anthonylife@anthonylife.(none)>
2013-04-30 09:57:54 +0800

    new file:   ctdict/paper_wordmap.py

---
+++ ctdict/paper_wordmap.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#encoding=utf8
+
+
+def main():
+
+
+if __name__ == "__main__":
+    paperinfo = "../../data/Paper.csv"
+    titlemap  = "../../dict/Titlemap"
+    keywdmap  = "../../dict/Keywdmap"
+
+    main()
