b13b428a0bbcaa78c86f2bc9d01c597b557fd013
anthonylife <anthonylife@anthonylife.(none)>
2013-05-08 08:48:43 +0800

    modified:   features/mkFeatures.py 	modified:   features/mkTitleSim.py 	deleted:    ctdict/paper_wordmap.py 	modified:   ctdict/wordmap.py

--- features/mkFeatures.py
+++ features/mkFeatures.py
@@ -5,6 +5,7 @@ import csv
 import sys
 import json
 import random
+from mkTitleSim import TitleSim
 
 
 def list_to_dict(ins):
@@ -28,6 +29,52 @@ def dict_to_list(item_dict, item_id):
     return item_list
 
 
+def sub_dict(somedict, somekeys):
+    return dict([(key, somedict.get(key)) for key in somekeys])
+
+
+def select_doc(prob_dict1, prob_dict2=None):
+    author_doc = {}
+    for key in prob_dict1.keys():
+        if prob_dict2 != None:
+            if key not in prob_dict2.keys():
+                print 'Key not match.'
+                sys.exit(1)
+            temp_list = prob_dict1[key] + prob_dict2[key]
+            temp_list = sorted(temp_list, key = lambda x:x[1], reverse=True)
+            topk = len(temp_list)/3+1
+            author_doc[key] = [entry[0] for entry in temp_list[:topk]]
+        else:
+
+    return author_doc
+
+
+def mkpairfeatures(titleSim, paths, pos_dict, neg_dict, author_set,\
+        pos_doc, neg_doc):
+    postr_subdict = sub_dict(pos_dict, author_set)
+    negtr_subdict = sub_dict(neg_dict, author_set)
+    postr_prob = titleSim.label_predict(postr_subdict)
+    negtr_prob = titleSim.label_predict(negtr_subdict)
+    author_doc = select_doc(postr_prob, negtr_prob)
+    features_conf = dict_to_list(pos_dict, author_set)
+    features_deleted = dict_to_list(neg_dict, author_set)
+    pos_titlesim = titleSim.calsim(author_doc, [train_pair[0:2] for train_pair in features_conf])
+    neg_titlesim = titleSim.calsim(author_doc, [train_pair[0:2] for train_pair in features_deleted])
+
+    newfeatures_conf = [entry1+entry2 for entry1,entry2 in zip(features_conf, pos_titlesim)]
+    newfeatures_deleted = [entry1+entry2 for entry1,entry2 in zip(features_deleted, neg_titlesim)]
+    writer = csv.writer(open(paths[pos_doc], 'w'), lineterminator="\n")
+    writer.writerows(newfeatures_conf)
+    writer = csv.writer(open(paths[neg_doc], 'w'), lineterminator="\n")
+    writer.writerows(newfeatures_deleted)
+
+
+def mksinglefeatures(titleSim, paths, test_features, test_dict, test_doc):
+    test_prob = titleSim.label_predict(test_dict)
+    author_doc = select(test_prob)
+    test_titlesim = titleSim.calsim(author_doc, [test_pair[0:2] for test_pair in test_features])
+
+
 def main():
     # reading paths
     print "Reading file path from the configuration file 'SETTINGS.json'"
@@ -36,6 +83,7 @@ def main():
     negtr_doc = paths["negtr_doc"]
     vali_doc  = paths["vali_doc"]
 
+    # loading instances
     print "Loading labeled instances."
     postr_ins = [ins[:-1] for ins in csv.reader(open(postr_doc))]
     negtr_ins = [ins[:-1] for ins in csv.reader(open(negtr_doc))]
@@ -50,12 +98,23 @@ def main():
     authors = set([entry[0] for entry in postr_ins])
     random.shuffle(authors)
     init_set = authors[0:len(authors)/3]
-    train_set = authors[len(authors)/3+1:len(authors)*2/3]
-    test_set = authors[len(authors)*2/3+1:len(authors)]
+    train_set = authors[len(authors)/3+1:len(authors)*3/4]
+    test_set = authors[len(authors)*3/4+1:len(authors)]
 
+    # making the first feature: title similarity
     print "Making title similarity features."
     features_conf = dict_to_list(postr_dict, init_set)
     features_deleted = dict_to_list(negtr_dict, init_set)
+    titleSim = TitleSim(features_conf[2:], features_deleted[2:])
+
+    mkpairfeatures(titleSim,paths,postr_dict,negtr_dict,train_set,\
+            "new_postr_doc","new_negtr_doc")
+
+    mkpairfeatures(titleSim,paths,postr_dict,negtr_dict,test_set,\
+            "new_poste_doc","new_negte_doc")
+
+    mksinglefeatures(titleSim, paths, vali_dict, vali_ins, "new_vali_doc")
+
 
 
 if __name__ == "__main__":
--- features/mkTitleSim.py
+++ features/mkTitleSim.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 #encoding=utf8
 
+import sys
+sys.path.append('../')
 import csv, json
+from nltk.stem import PorterStemmer
 from skleanr.ensemble import RandomForestClassifier
 
 class TitleSim:
     def __init__(self, features_conf, features_deleted):
+        print 'Start initialization'
+
         # initial model training
         features = features_deleted + features_conf
         target = [0 for x in range(len(features_deleted))] +\
@@ -22,12 +27,68 @@ class TitleSim:
         paper_doc = paths["paper_doc"]
         self.paper = dict([(entry[0], entry[1]) for entry in csv.reader(open(paper_doc))])
 
+        # loading setting file
+        self.paths = json.loads(open("SETTINGS.json").read())
+
+        # loading word map of titles
+        self.wordmap = self.load_titlemap()
+
+        # do other initializations
+        self.stemmer = PorterStemmer()
+        print 'End initialization'
+
 
     def label_predict(self, fea_dict):
         # fea_dict is a dictionary whose key is 'user id'
         prob_dict = {}
-
         for key in fea_dict:
             features = [feature[1:] for feature in fea_dict[key]]
+            predictions = self.classifier.predict_proba(features)[:,1]
+            prob_dict[key]=[(item[0],prob) for item,prob in zip(fea_dict[key],predictions)]
+        return prob_dict
 
-    def calsim(self, )
+
+    def load_titlemap(self):
+        return dict([(entry[0],entry[1]) for entry in \
+                csv.reader(open(self.paths["title_wordmap"]))])
+
+    def calsim(self, author_doc, pairs):
+        # calculate the similarity between titles
+        title_features = []
+        for pair in pairs:
+            if pair[0] not in author_doc:
+                print 'Key error.'
+                sys.exit(1)
+            title_features += self.calpairsim(author_doc[pair[0]], pair[1])
+
+        return title_features
+
+    def calpairsim(self, doclist, target_doc):
+        author_words = {}
+        for doc in doclist:
+            words = self.paper[doc].lower().split(' ')
+            for word in words:
+                stemmed_word = self.stemmer.stem_word(word)
+                if stemmed_word in self.wordmap:
+                    if stemmed_word in author_words:
+                        author_words[stemmed_word] += 1
+                    else:
+                        author_words[stemmed_word] = 1
+
+        doc_words = {}
+        words = self.paper[target_doc].lower().split(' ')
+        for word in words:
+            stemmed_word = self.stemmer.stem_word(word)
+            if stemmed_word in self.wordmap:
+                if stemmed_word in doc_words:
+                    doc_words[stemmed_word] += 1
+                else:
+                    doc_words[stemmed_word] = 1
+
+        # number of common words
+        comm_num = len(set(author_words.keys()) & set(doc_words.keys()))
+
+        # pearson coefficient
+        pearson = comm_num*1.0/ (len(set(author_words.keys())) + len(set(doc_words.keys())))
+
+        return [comm_num, pearson]
